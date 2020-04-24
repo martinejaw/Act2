2a3aafd9929d4ee51f5513b8cb13a8d35dded888
martinejaw <martinezequieljaworski@gmail.com>
2020-04-24 10:09:09 -0300

    Version terminada

--- Entidades/Medico.cs
+++ Entidades/Medico.cs
@@ -5,11 +5,16 @@ namespace EFGetStarted.Entidades
 {    
     public class Medico
     {
+
+        public Medico(){
+            this.casos = new HashSet<Caso>();
+        }
+
         [Key]
         public int matricula { get; set; }
         public string nombre { get; set; }
         public string apellido { get; set; }
-        public List<Consulta> consultas { get; set; }
-        public List<Caso> casos { get; set; }
+        public List<Consulta> consultas { get; set; } = new List<Consulta>();
+        public virtual ICollection<Caso> casos { get; set; }
     }
 }
--- Programa.cs
+++ Programa.cs
@@ -8,38 +8,43 @@ namespace EFGetStarted
     {
         static void Main()
         {
+
+            
+
+            var paciente1 = new Paciente { dni = 41000001 ,
+                                           nombre = "Martin",
+                                            apellido = "Yaoming"};
+            var paciente2 = new Paciente { dni = 41000002 ,
+                                            nombre = "Juan",
+                                            apellido = "Bo"};
+            var paciente3 = new Paciente { dni = 41000003 ,
+                                            nombre = "Jazmin",
+                                            apellido = "Flores"};
+
+
+            var medico1 = new Medico { matricula = 10001, nombre = "Ezequiel", apellido = "Del Peru"};
+            var medico2 = new Medico { matricula = 10002, nombre = "Hernan", apellido = "Troncoso"};
+
+            var caso1 = new Caso { estado = "En prueba", medico = medico1, paciente = paciente1};
+            var caso2 = new Caso { estado = "Sospechoso", medico = medico1, paciente = paciente2};
+            var caso3 = new Caso { estado = "Saludable", medico = medico2, paciente = paciente3};
+
+            var consulta1 = new Consulta { descripcionSintomas = "catarro, diarrea" };
+            var consulta2 = new Consulta { descripcionSintomas = "estornudos, fiebre" };
+            var consulta3 = new Consulta { descripcionSintomas = "estornudos, fiebre" };
+
+            medico1.consultas.Add(consulta1);
+            medico1.consultas.Add(consulta2);
+            medico2.consultas.Add(consulta3);
+            paciente1.consultas.Add(consulta1);
+            paciente2.consultas.Add(consulta2);
+            paciente3.consultas.Add(consulta3);
+
+
             using (var db = new ContextoHospital())
             {
 
-
-                var paciente1 = new Paciente { 
-                                                nombre = "Martin",
-                                                apellido = "Yaoming"};
-                var paciente2 = new Paciente { dni = 41000002 ,
-                                                nombre = "Juan",
-                                                apellido = "Bo"};
-                var paciente3 = new Paciente { dni = 41000003 ,
-                                                nombre = "Jazmin",
-                                                apellido = "Flores"};
-
-
-                var medico1 = new Medico { matricula = 1, nombre = "Ezequiel", apellido = "Del Peru"};
-                var medico2 = new Medico { matricula = 2, nombre = "Hernan", apellido = "Troncoso"};
-
-                var caso1 = new Caso { estado = "En prueba", medico = medico1, paciente = paciente1};
-                var caso2 = new Caso { estado = "Sospechoso", medico = medico1, paciente = paciente2};
-                var caso3 = new Caso { estado = "En prueba", medico = medico2, paciente = paciente3};
-
-                //medicoNuevo.casos.Add(casoNuevo);
-
-                //casoNuevo.medico = medicoNuevo;
-                
-                /*Console.WriteLine("Insertando un caso");
-                db.Add(casoNuevo);
-                db.Add(pacienteNuevo);
-                db.Add(medicoNuevo);
-                db.SaveChanges();*/
-
+                //Insercion de en la base de datos, esto solo debe hacerse una vez
                 db.Add(caso1);
                 db.Add(caso2);
                 db.Add(caso3);
@@ -48,10 +53,13 @@ namespace EFGetStarted
                 db.Add(paciente3);
                 db.Add(medico1);
                 db.Add(medico2);
+                db.Add(consulta1);
+                db.Add(consulta2);
+                db.Add(consulta3);
                 db.SaveChanges();
 
 
-                Console.WriteLine("Leyendo un caso");
+                Console.WriteLine("Listando todos los casos en la BBDD");
                 var casos = db.Casos
                     .OrderBy(c => c.CasoId)
                     .ToList();
@@ -63,9 +71,44 @@ namespace EFGetStarted
                     Console.WriteLine(caso.pacientedni);
                     Console.WriteLine("Medico encargado: ");
                     Console.WriteLine(caso.medicomatricula);
+                    Console.WriteLine("////////////////////////////");
                 }
                 
+                Console.WriteLine("////////////////////////////");
+                Console.WriteLine("----------------------------");
+                Console.WriteLine("////////////////////////////");
+            
 
+                Console.WriteLine("Leyendo las consultas asignadas al medico1");
+                var consultas = db.Consultas
+                    .Where(c => c.medicomatricula == medico1.matricula)
+                    .OrderBy(c => c.ConsultaId)
+                    .ToList();
+
+                foreach (Consulta consulta in consultas){
+                    Console.WriteLine("Numero de consulta: ");
+                    Console.WriteLine(consulta.ConsultaId);
+                    Console.WriteLine("Paciente del caso: ");
+                    Console.WriteLine(consulta.pacientedni);
+                    Console.WriteLine("Medico encargado: ");
+                    Console.WriteLine(consulta.medicomatricula);
+                    Console.WriteLine("////////////////////////////");
+                }
+
+
+
+                //Ejemplo actualizacion
+                Console.WriteLine("Medico1 de licencia, se asigna al medico2 a los casos del medico1");
+                var casosAsignar = db.Casos
+                    .Where(c => c.medicomatricula == 10001)
+                    .ToList();
+
+                foreach (Caso casoa in casosAsignar){
+                    Console.WriteLine("Asignando caso {0} a medico2",casoa.CasoId);
+                    casoa.medicomatricula = 10002;
+                    db.SaveChanges();
+                }
+                
             }
         }
     }
